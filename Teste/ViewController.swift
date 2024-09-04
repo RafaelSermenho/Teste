c6abc42d1c89ef366bf4d9ff64e96b881689201a
rafaelsermenho <rafael.sermenho@gmail.com>
2024-09-04 19:50:30 -0300

    Initial Commit

---
+++ Teste/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Teste
+//
+//  Created by Rafael Sermenho Pedroso on 04/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
